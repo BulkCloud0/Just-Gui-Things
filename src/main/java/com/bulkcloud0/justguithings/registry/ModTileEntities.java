fd3cc8092b8f704d4e4b680236934400663451c9
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-17 09:35:33 -0300

    feat: register Crusher tile entity

--- src/main/java/com/bulkcloud0/justguithings/registry/ModTileEntities.java
+++ src/main/java/com/bulkcloud0/justguithings/registry/ModTileEntities.java
@@ -2,6 +2,7 @@ package com.bulkcloud0.justguithings.registry;
 
 import com.bulkcloud0.justguithings.JustGuiThings;
 import com.bulkcloud0.justguithings.world.tile.CoalGeneratorTileEntity;
+import com.bulkcloud0.justguithings.world.tile.CrusherTileEntity;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,6 +14,10 @@ public final class ModTileEntities {
     public static final RegistryObject<TileEntityType<CoalGeneratorTileEntity>> COAL_GENERATOR = TILE_ENTITIES.register(
             "coal_generator",
             () -> TileEntityType.Builder.of(CoalGeneratorTileEntity::new, ModBlocks.COAL_GENERATOR.get()).build(null));
+
+    public static final RegistryObject<TileEntityType<CrusherTileEntity>> CRUSHER = TILE_ENTITIES.register(
+            "crusher",
+            () -> TileEntityType.Builder.of(CrusherTileEntity::new, ModBlocks.CRUSHER.get()).build(null));
 
     private ModTileEntities() {}
 }
