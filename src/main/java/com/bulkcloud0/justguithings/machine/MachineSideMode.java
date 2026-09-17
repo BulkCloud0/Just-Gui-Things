61451460a2536a878bdb491e8435cf86c1955a6b
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-17 10:22:27 -0300

    feat: add machine side configuration modes

---
+++ src/main/java/com/bulkcloud0/justguithings/machine/MachineSideMode.java
@@ -0,0 +1,21 @@
+package com.bulkcloud0.justguithings.machine;
+
+public enum MachineSideMode {
+    DISABLED,
+    INPUT,
+    OUTPUT,
+    ENERGY;
+
+    public MachineSideMode next() {
+        MachineSideMode[] values = values();
+        return values[(ordinal() + 1) % values.length];
+    }
+
+    public static MachineSideMode fromOrdinal(int ordinal) {
+        MachineSideMode[] values = values();
+        if (ordinal < 0 || ordinal >= values.length) {
+            return DISABLED;
+        }
+        return values[ordinal];
+    }
+}
