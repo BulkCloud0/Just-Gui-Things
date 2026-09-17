a430fb8a3795f4f5063946a7841f419a72f635e2
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-17 09:31:31 -0300

    fix: use 1.16.5 IIntArray package in generator container

--- src/main/java/com/bulkcloud0/justguithings/world/container/CoalGeneratorContainer.java
+++ src/main/java/com/bulkcloud0/justguithings/world/container/CoalGeneratorContainer.java
@@ -5,12 +5,11 @@ import com.bulkcloud0.justguithings.world.tile.CoalGeneratorTileEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.container.Container;
-import net.minecraft.inventory.container.IContainerListener;
-import net.minecraft.inventory.container.IIntArray;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIntArray;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.items.SlotItemHandler;
 
