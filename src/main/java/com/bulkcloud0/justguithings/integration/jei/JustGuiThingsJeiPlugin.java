de6792f52512f58232232cf9c8df8fa37313458c
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-17 11:11:38 -0300

    feat: register Crusher recipes with JEI

---
+++ src/main/java/com/bulkcloud0/justguithings/integration/jei/JustGuiThingsJeiPlugin.java
@@ -0,0 +1,51 @@
+package com.bulkcloud0.justguithings.integration.jei;
+
+import com.bulkcloud0.justguithings.JustGuiThings;
+import com.bulkcloud0.justguithings.client.screen.CrusherScreen;
+import com.bulkcloud0.justguithings.registry.ModItems;
+import com.bulkcloud0.justguithings.registry.ModRecipes;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+
+@JeiPlugin
+public final class JustGuiThingsJeiPlugin implements IModPlugin {
+    private static final ResourceLocation PLUGIN_UID = new ResourceLocation(JustGuiThings.MOD_ID, "jei_plugin");
+
+    @Override
+    public ResourceLocation getPluginUid() {
+        return PLUGIN_UID;
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        registration.addRecipeCategories(new CrusherRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        ClientWorld world = Minecraft.getInstance().level;
+        if (world != null) {
+            registration.addRecipes(
+                    world.getRecipeManager().getAllRecipesFor(ModRecipes.CRUSHING_TYPE),
+                    CrusherRecipeCategory.UID);
+        }
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(ModItems.CRUSHER.get()), CrusherRecipeCategory.UID);
+    }
+
+    @Override
+    public void registerGuiHandlers(IGuiHandlerRegistration registration) {
+        registration.addRecipeClickArea(CrusherScreen.class, 69, 38, 36, 12, CrusherRecipeCategory.UID);
+    }
+}
