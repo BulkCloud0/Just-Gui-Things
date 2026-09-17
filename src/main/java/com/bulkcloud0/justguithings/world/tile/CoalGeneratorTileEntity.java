5413c6166bf248f1f7130a2bffe913d469f15c01
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-17 09:33:33 -0300

    fix: import INamedContainerProvider for generator menu

--- src/main/java/com/bulkcloud0/justguithings/world/tile/CoalGeneratorTileEntity.java
+++ src/main/java/com/bulkcloud0/justguithings/world/tile/CoalGeneratorTileEntity.java
@@ -7,6 +7,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.container.Container;
+import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
