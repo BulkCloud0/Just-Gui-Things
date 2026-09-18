9eed7da2b8c8e63fc062af0154a8ffc025c87870
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-18 18:19:59 -0300

    feat: show configurator target in hud

--- src/main/java/com/bulkcloud0/justguithings/item/ConfiguratorItem.java
+++ src/main/java/com/bulkcloud0/justguithings/item/ConfiguratorItem.java
@@ -1,22 +1,10 @@
 package com.bulkcloud0.justguithings.item;
 
-import com.bulkcloud0.justguithings.logistics.ConduitTransferMode;
-import com.bulkcloud0.justguithings.machine.BaseMachineTileEntity;
-import com.bulkcloud0.justguithings.world.tile.BasicEnergyCableTileEntity;
-import com.bulkcloud0.justguithings.world.DirectionText;
-import com.bulkcloud0.justguithings.world.block.AbstractConduitBlock;
-import com.bulkcloud0.justguithings.world.tile.BasicFluidPipeTileEntity;
-import com.bulkcloud0.justguithings.world.tile.BasicItemPipeTileEntity;
-import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemUseContext;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Direction;
 import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TranslationTextComponent;
-
-import javax.annotation.Nullable;
 
 public class ConfiguratorItem extends TooltipItem {
     public ConfiguratorItem(Properties properties) {
@@ -34,16 +22,15 @@ public class ConfiguratorItem extends TooltipItem {
         }
 
         TileEntity tile = context.getLevel().getBlockEntity(context.getClickedPos());
-        if (!(tile instanceof BasicItemPipeTileEntity)
-                && !(tile instanceof BasicFluidPipeTileEntity)
-                && !(tile instanceof BasicEnergyCableTileEntity)
-                && !(tile instanceof BaseMachineTileEntity)) {
+        if (!ConfiguratorTargetDescription.isSupported(tile)) {
             return ActionResultType.PASS;
         }
 
         if (!context.getLevel().isClientSide) {
-            BlockState state = context.getLevel().getBlockState(context.getClickedPos());
-            ITextComponent inspection = getInspectionText(tile, state, context.getClickedFace());
+            ITextComponent inspection = ConfiguratorTargetDescription.getDescription(
+                    tile,
+                    context.getLevel().getBlockState(context.getClickedPos()),
+                    context.getClickedFace());
             if (inspection != null) {
                 player.displayClientMessage(inspection, false);
             }
@@ -51,49 +38,4 @@ public class ConfiguratorItem extends TooltipItem {
 
         return context.getLevel().isClientSide ? ActionResultType.SUCCESS : ActionResultType.CONSUME;
     }
-
-    @Nullable
-    private ITextComponent getInspectionText(TileEntity tile, BlockState state, Direction direction) {
-        ITextComponent face = DirectionText.getDisplayName(direction);
-        ITextComponent connection = getConnectionStateName(
-                AbstractConduitBlock.isConnected(state, direction));
-
-        if (tile instanceof BasicItemPipeTileEntity) {
-            ConduitTransferMode mode = ((BasicItemPipeTileEntity) tile).getSideMode(direction);
-            return new TranslationTextComponent(
-                    "message.justguithings.configurator.inspect_item_pipe",
-                    face, mode.getDisplayName(), connection);
-        }
-
-        if (tile instanceof BasicFluidPipeTileEntity) {
-            ConduitTransferMode mode = ((BasicFluidPipeTileEntity) tile).getSideMode(direction);
-            return new TranslationTextComponent(
-                    "message.justguithings.configurator.inspect_fluid_pipe",
-                    face, mode.getDisplayName(), connection);
-        }
-
-        if (tile instanceof BasicEnergyCableTileEntity) {
-            ConduitTransferMode mode = ((BasicEnergyCableTileEntity) tile).getSideMode(direction);
-            return new TranslationTextComponent(
-                    "message.justguithings.configurator.inspect_energy_cable",
-                    face, mode.getEnergyDisplayName(), connection);
-        }
-
-        if (tile instanceof BaseMachineTileEntity) {
-            BaseMachineTileEntity machine = (BaseMachineTileEntity) tile;
-            return new TranslationTextComponent(
-                    "message.justguithings.configurator.inspect_machine",
-                    face, machine.getSideMode(direction).getDisplayName());
-        }
-
-        return null;
-    }
-
-    private ITextComponent getConnectionStateName(boolean connected) {
-        return new TranslationTextComponent(
-                connected
-                        ? "connection.justguithings.connected"
-                        : "connection.justguithings.disconnected");
-    }
-
 }
