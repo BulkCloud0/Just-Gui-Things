a533d107810871c5577a5c142677e4b84d15e7f5
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-18 09:02:11 -0300

    refactor: migrate Coal Generator to machine core

--- src/main/java/com/bulkcloud0/justguithings/world/block/CoalGeneratorBlock.java
+++ src/main/java/com/bulkcloud0/justguithings/world/block/CoalGeneratorBlock.java
@@ -1,60 +1,9 @@
 package com.bulkcloud0.justguithings.world.block;
 
-import com.bulkcloud0.justguithings.machine.MachineInventoryDropHelper;
 import com.bulkcloud0.justguithings.world.tile.CoalGeneratorTileEntity;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.world.IBlockReader;
-import net.minecraft.world.World;
-import net.minecraftforge.fml.network.NetworkHooks;
 
-import javax.annotation.Nullable;
-
-public class CoalGeneratorBlock extends Block {
+public class CoalGeneratorBlock extends BaseMachineBlock<CoalGeneratorTileEntity> {
     public CoalGeneratorBlock(Properties properties) {
-        super(properties);
-    }
-
-    @Override
-    public boolean hasTileEntity(BlockState state) {
-        return true;
-    }
-
-    @Nullable
-    @Override
-    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
-        return new CoalGeneratorTileEntity();
-    }
-
-    @Override
-    public void onRemove(BlockState state, World world, BlockPos pos, BlockState newState, boolean isMoving) {
-        if (state.getBlock() != newState.getBlock()) {
-            TileEntity tile = world.getBlockEntity(pos);
-            if (tile instanceof CoalGeneratorTileEntity) {
-                MachineInventoryDropHelper.dropContents(world, pos, ((CoalGeneratorTileEntity) tile).getInventory());
-            }
-        }
-        super.onRemove(state, world, pos, newState, isMoving);
-    }
-
-    @Override
-    public ActionResultType use(BlockState state, World world, BlockPos pos, PlayerEntity player,
-                                Hand hand, BlockRayTraceResult hit) {
-        if (!world.isClientSide) {
-            TileEntity tile = world.getBlockEntity(pos);
-            if (tile instanceof CoalGeneratorTileEntity && player instanceof ServerPlayerEntity) {
-                NetworkHooks.openGui((ServerPlayerEntity) player, (CoalGeneratorTileEntity) tile, pos);
-            }
-            return ActionResultType.CONSUME;
-        }
-
-        return ActionResultType.SUCCESS;
+        super(properties, CoalGeneratorTileEntity.class, CoalGeneratorTileEntity::new);
     }
 }
