c1e79d6d79c94d3f8530971315722670bd0252bf
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-18 09:18:38 -0300

    refactor: share single-input processing recipe base

--- src/main/java/com/bulkcloud0/justguithings/recipe/CrusherRecipe.java
+++ src/main/java/com/bulkcloud0/justguithings/recipe/CrusherRecipe.java
@@ -3,66 +3,19 @@ package com.bulkcloud0.justguithings.recipe;
 import com.bulkcloud0.justguithings.registry.ModRecipes;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.IRecipeSerializer;
 import net.minecraft.item.crafting.IRecipeType;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.JSONUtils;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.World;
 import net.minecraftforge.registries.ForgeRegistryEntry;
 
 import javax.annotation.Nullable;
 
-public class CrusherRecipe implements IRecipe<IInventory>, MachineProcessingRecipe {
-    private final ResourceLocation id;
-    private final Ingredient input;
-    private final RecipeOutput result;
-    private final int processingTime;
-    private final int energyPerTick;
-
+public class CrusherRecipe extends SingleInputProcessingRecipe {
     public CrusherRecipe(ResourceLocation id, Ingredient input, RecipeOutput result, int processingTime, int energyPerTick) {
-        this.id = id;
-        this.input = input;
-        this.result = result;
-        this.processingTime = processingTime;
-        this.energyPerTick = energyPerTick;
-    }
-
-    @Override
-    public boolean matches(IInventory inventory, World world) {
-        return input.test(inventory.getItem(0));
-    }
-
-    @Override
-    public ItemStack assemble(IInventory inventory) {
-        return result.resolve(inventory.getItem(0));
-    }
-
-    @Override
-    public boolean canCraftInDimensions(int width, int height) {
-        return width >= 1 && height >= 1;
-    }
-
-    @Override
-    public ItemStack getResultItem() {
-        return result.resolve();
-    }
-
-    @Override
-    public NonNullList<Ingredient> getIngredients() {
-        NonNullList<Ingredient> ingredients = NonNullList.create();
-        ingredients.add(input);
-        return ingredients;
-    }
-
-    @Override
-    public ResourceLocation getId() {
-        return id;
+        super(id, input, result, processingTime, energyPerTick);
     }
 
     @Override
@@ -73,22 +26,6 @@ public class CrusherRecipe implements IRecipe<IInventory>, MachineProcessingReci
     @Override
     public IRecipeType<?> getType() {
         return ModRecipes.CRUSHING_TYPE;
-    }
-
-    public Ingredient getInput() {
-        return input;
-    }
-
-    public java.util.List<ItemStack> getResultDisplayStacks() {
-        return result.getDisplayStacks();
-    }
-
-    public int getProcessingTime() {
-        return processingTime;
-    }
-
-    public int getEnergyPerTick() {
-        return energyPerTick;
     }
 
     public static class Serializer extends ForgeRegistryEntry<IRecipeSerializer<?>> implements IRecipeSerializer<CrusherRecipe> {
