fdb0219e04b2c8aa6d2f769cf7789d81362a132b
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-19 11:25:12 -0300

    refactor: bridge legacy module type ids

--- src/main/java/com/bulkcloud0/justguithings/machine/module/MachineModuleTypes.java
+++ src/main/java/com/bulkcloud0/justguithings/machine/module/MachineModuleTypes.java
@@ -1,18 +1,24 @@
 package com.bulkcloud0.justguithings.machine.module;
 
-import com.bulkcloud0.justguithings.JustGuiThings;
 import net.minecraft.util.ResourceLocation;
 
+/**
+ * @deprecated Use
+ * {@link com.bulkcloud0.justguithings.api.machine.module.MachineModuleTypes}.
+ */
+@Deprecated
 public final class MachineModuleTypes {
-    public static final ResourceLocation SPEED = id("speed");
-    public static final ResourceLocation EFFICIENCY = id("efficiency");
-    public static final ResourceLocation BUFFER = id("buffer");
-    public static final ResourceLocation BATCH = id("batch");
-    public static final ResourceLocation POWER_COIL = id("power_coil");
+    public static final ResourceLocation SPEED =
+            com.bulkcloud0.justguithings.api.machine.module.MachineModuleTypes.SPEED;
+    public static final ResourceLocation EFFICIENCY =
+            com.bulkcloud0.justguithings.api.machine.module.MachineModuleTypes.EFFICIENCY;
+    public static final ResourceLocation BUFFER =
+            com.bulkcloud0.justguithings.api.machine.module.MachineModuleTypes.BUFFER;
+    public static final ResourceLocation BATCH =
+            com.bulkcloud0.justguithings.api.machine.module.MachineModuleTypes.BATCH;
+    public static final ResourceLocation POWER_COIL =
+            com.bulkcloud0.justguithings.api.machine.module.MachineModuleTypes.POWER_COIL;
 
-    private MachineModuleTypes() {}
-
-    private static ResourceLocation id(String path) {
-        return new ResourceLocation(JustGuiThings.MOD_ID, path);
+    private MachineModuleTypes() {
     }
 }
